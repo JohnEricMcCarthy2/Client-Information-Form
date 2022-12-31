060d01e7a140f3c594f1e72d09bcbc5be99a4094
JohnEricMcCarthy2 <j.mccarthy@combocurve.com>
2022-12-31 17:59:29 -0600

    update console message

--- index.js
+++ index.js
@@ -55,4 +55,4 @@ app.post("/", async (req, res) => {
   res.send("Successfully submitted! Thank you!");
 });
 
-app.listen(1337, (req, res) => console.log("running on 1337"));
+app.listen(1337, (req, res) => console.log("running on localhost:1337"));
